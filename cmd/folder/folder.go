ad77b73a2aa47ba9ce71d125410957e84308ad43
awe8128 <dailywork8128@gmail.com>
2025-12-13 16:53:39 +0900

    add: sample commands

---
+++ cmd/folder/folder.go
@@ -0,0 +1,34 @@
+package folder
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	GenerateFolderCmd = &cobra.Command{
+		Use:   "folder",
+		Short: "Generate folders for system design",
+		Long: `Create folder structure for specific system design
+for example DDD
+/domain
+	/[domain-name]
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Running generate folder structure command")
+		},
+	}
+)
+
+func Execute() {
+	err := GenerateFolderCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+
+}
